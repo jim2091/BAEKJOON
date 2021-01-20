72ebfad8102f486691a693e87a319b48e82a1172
jim2091 <69296289+jim2091@users.noreply.github.com>
2021-01-20 17:35:42 +0900

    백준 3일치

---
+++ 10172.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 16:40:06 2021
+
+@author: LJB
+"""
+
+print('|\\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
---
+++ 2557.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 16:19:15 2021
+
+@author: LJB
+"""
+
+print("Hello World!")
