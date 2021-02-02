e4662590d5c6ff3e1e7fc45d6cb8cd4ea4b70b2d
jim2091 <69296289+jim2091@users.noreply.github.com>
2021-02-02 12:23:29 +0900

    2단계 4번

---
+++ 14681.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 12:17:16 2021
+
+@author: LJB
+"""
+
+x = int(input())
+y = int(input())
+
+if x > 0:
+    if y > 0:
+        print('1')
+    elif y < 0:
+        print('4')
+elif x < 0:
+    if y > 0:
+        print('2')
+    elif y < 0:
+        print('3')
