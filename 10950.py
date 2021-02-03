fa34322e1536505441a85181a29bd8baf73df697
jim2091 <69296289+jim2091@users.noreply.github.com>
2021-02-03 12:41:47 +0900

    3단계 2번

---
+++ 10950.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  3 12:32:02 2021
+
+@author: LJB
+"""
+
+T = int(input())
+
+for i in range(T):
+    A, B = map(int, input().split())
+    print(A + B)
